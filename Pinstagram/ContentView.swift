044b21740230b8557f65c1bc1150a9a40f5b8999
Robert Pelka <123455ff@gmail.com>
2021-10-03 13:01:19 +0200

    Initial Commit

---
+++ Pinstagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pinstagram
+//
+//  Created by Robert Pelka on 03/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pinstagram/PinstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinstagramApp.swift
+//  Pinstagram
+//
+//  Created by Robert Pelka on 03/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PinstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
