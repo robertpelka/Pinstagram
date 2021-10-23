368e9dfea1013418a3f6f2ef2a65b9264f45842e
Robert Pelka <123455ff@gmail.com>
2021-10-23 09:28:48 +0200

    Add Firebase

--- Pinstagram/PinstagramApp.swift
+++ Pinstagram/PinstagramApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct PinstagramApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
