21c1a425aed8f19449043c6acf53e597a8ae3ed2
Robert Pelka <123455ff@gmail.com>
2021-10-27 16:53:30 +0200

    Add User model

---
+++ Pinstagram/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Pinstagram
+//
+//  Created by Robert Pelka on 23/10/2021.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: String
+    let username: String
+    var profileImage: String
+    var bio: String = "Hello, I'm new here 😊"
+    var visitedCountries: Int = 0
+    var followers: Int = 0
+    var following: Int = 0
+}
